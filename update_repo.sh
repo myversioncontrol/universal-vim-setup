d59768259370688f6a1143fdb684d1dee91de7fd
root <root@liam.datatill.com>
2018-06-08 09:19:19 +0200

    added two simple little scripts so I don't have to make changes manually

---
+++ update_repo.sh
@@ -0,0 +1,3 @@
+cp .vimrc ~/.vimrc
+cp ~/.vimrc ~/.vimrc.old
+echo "And that's the Kansas City Shuffle..."
---
+++ update_vimrc.sh
@@ -0,0 +1,3 @@
+cp ~/.vimrc ~/.vimrc.old
+cp .vimrc ~/.vimrc
+echo "And that's the Kansas City Shuffle..."
